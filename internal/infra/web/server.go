295988fa293cc9e9c2b86f76d31a3847a7c20e31
Adriano Stankewicz <adriano.stankewicz@gmail.com>
2025-10-17 23:22:42 -0300

    feat(package): rename packages in the web path

--- internal/infra/web/server.go
+++ internal/infra/web/server.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	customer "github.com/adrianostankewicz/customer-favorites/internal/customer/service"
-	"github.com/adrianostankewicz/customer-favorites/internal/infra/web"
+	"github.com/adrianostankewicz/customer-favorites/internal/infra/web/handler"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -27,13 +27,13 @@ func (w *WebServer) AddHandler(s *customer.CustomerService) *chi.Mux {
 	w.Router.Use(middleware.Logger)
 	w.Router.Use(middleware.Recoverer)
 
-	customerHandler := web.NewWebCustomerHandler(s)
+	customerHandler := handler.NewWebCustomerHandler(s)
 
 	w.Router.Route("/customers", func(r chi.Router) {
 		r.Post("/", customerHandler.Create)
-		r.Get("/{id}", customerHandler.FindByID)
-		r.Put("/{id}", customerHandler.Update)
-		r.Delete("/{id}", customerHandler.Delete)
+		//r.Get("/{id}", customerHandler.FindByID)
+		//r.Put("/{id}", customerHandler.Update)
+		//r.Delete("/{id}", customerHandler.Delete)
 	})
 
 	return w.Router
